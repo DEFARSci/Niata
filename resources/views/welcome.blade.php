0e179132b6a9c5a345309427892c6eb4846fb0d4
mamejarrah <diarragueye799@gmail.com>
2023-07-14 16:20:24 +0000

    commit blog 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,13 @@
 @extends('layout.app')
 
 @section('content')
-
-<h1> Niata</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Bienvenue sur notre site</h1>
+            <p>Ceci est notre page d'accueil.</p>
+            <a href="{{route('blog')}}" class="btn btn-primary">Voir les actualités</a>
+        </div>
+    </div>
+</div>
 @endsection
