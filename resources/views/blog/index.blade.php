282a9b4d6c31ffe1be1c758513fe128fb15b90fe
Dammel-Meuz <ndoyemamemoussa@gmail.com>
2023-07-14 16:02:10 +0000

    updat blog modifier2

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -30,22 +30,27 @@
 
 <a href="{{route('create')}}"> Ajouter un article </a>
 
-<h1> NOTRE BLOG</h1>
-
-<div class="container">
+<div class="container-fluide">
+<div class="row">
+<div class="col-md-2"> </div>
+<div class="col-md-8">
     <div class="row">
-        @foreach ($blog as $blog )
-
-<div class="card p-3" style="width: 18rem;">
-    <img src="{{asset('images/'.$blog->image)}}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">{{$blog->title}}</h5>
-      <a href="#" class="btn btn-primary">show</a>
+    @foreach ($blog as $blog)
+    <div class="col-md-3 mb-4"> <!-- Chaque carte occupe 3 colonnes sur une grille de 12 colonnes -->
+        <div class="card" style="width: 100%;">
+            <img src="{{asset('images/'.$blog->image)}}" class="card-img-top" style="height: 100px; object-fit: cover;" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{$blog->title}}</h5>
+                <h6 class="card-text" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{$blog->text}}</h6>
+                <a href="{{route('blog.show', $blog->id)}}" class="btn btn-primary">show</a>
+            </div>
+        </div>
     </div>
-  </div>
-    
-@endforeach
+    @endforeach
+</div>
     </div>
+    <div class="col-md-2"> </div>
+</div>
 </div>
 <style>
   .carousel-image {
