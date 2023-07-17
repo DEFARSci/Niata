75dbeb5cdb276eefcdd3ec25e2bf81faec71839b
Meuz Ndoye <70266170+Dammel-Meuz@users.noreply.github.com>
2023-07-17 15:03:57 +0000

    Update index.blade.php

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -30,26 +30,26 @@
 
 
 <div class="container-fluide">
-  <div class="row">
-    <div class="col-md-2"> </div>
-    <div class="col-md-8">
-      <div class="row">
-        @foreach ($blog as $blog)
-        <div class="col-md-3 mb-4">
-          <div class="card" style="width: 100%;">
+<div class="row">
+<div class="col-md-2"> </div>
+<div class="col-md-8">
+    <div class="row">
+    @foreach ($blog as $blog)
+    <div class="col-md-3 mb-4"> <!-- Chaque carte occupe 3 colonnes sur une grille de 12 colonnes -->
+        <div class="card" style="width: 100%;">
             <img src="{{asset('images/'.$blog->image)}}" class="card-img-top" style="height: 100px; object-fit: cover;" alt="...">
             <div class="card-body">
-              <h5 class="card-title">{{$blog->title}}</h5>
-              <h6 class="card-text" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{$blog->text}}</h6>
-              <a href="{{route('blog.show', $blog->id)}}" class="btn btn-primary">show</a>
+                <h5 class="card-title">{{$blog->title}}</h5>
+                <h6 class="card-text" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{$blog->text}}</h6>
+                <a href="{{route('blog.show', $blog->id)}}" class="btn btn-primary">show</a>
             </div>
-          </div>
         </div>
-        @endforeach
-      </div>
+    </div>
+    @endforeach
+</div>
     </div>
     <div class="col-md-2"> </div>
-  </div>
+</div>
 </div>
 
 <style>
